fa704999a2df92e9cbbc8a378f80d6c020652b5d
Rishcakes <rishitandon12@gmail.com>
2025-08-04 12:02:59 +0100

    Latest update in meeting

---
+++ working_analyses/mastersResearch_Jun_Sept2025/dimRed_P/Reducedstatistics/Q_NN.py
@@ -0,0 +1,52 @@
+import numpy as np
+from cell_auxiliary_functions import reducer_reader
+from coranking_auxiliary_functions import ranking_matrix, coranking_matrix, compute_QNN, compute_auc_QNN
+from sklearn.metrics import pairwise_distances
+from pyspoc import ReducedStatistic
+
+
+class Q_NN(ReducedStatistic):
+
+    def __init__(self, method = 'PCA', reduced_dimensionality = 2):
+
+        # Calling base class initialiser.
+        super().__init__()
+
+        self.method = method
+        self.reduced_dimensionality = reduced_dimensionality
+        self.reducer = reducer_reader(self.method)(self.reduced_dimensionality)
+
+    @property
+    def name(self) -> str:
+        return "Q_NN"
+
+    @property
+    def identifier(self) -> str:
+        return "my_new_reducer_identifier"
+
+    @property
+    def labels(self) -> list[str]:
+        return ["my_new_reducer_label_1",
+                "my_new_reducer_label_2",
+                "my_new_reducer_label_n"]
+
+    def compute(self, data: np.ndarray) -> float:
+
+        # Dimensionally reduce the data
+        X = data
+        Z = self.reducer.fit_transform(X)
+
+        # Construct the distance matrices
+        D  = pairwise_distances(X)
+        D_z = pairwise_distances(Z)
+
+        # Compute ranking and co-ranking matrices
+        R = ranking_matrix(D)
+        R_z = ranking_matrix(D_z)
+        Q = coranking_matrix(R, R_z)
+
+        # Compute QNN and take the AUC (average in this case)
+        QNN = compute_QNN(Q)
+        auc_QNN = compute_auc_QNN(QNN)
+
+        return auc_QNN
---
+++ working_analyses/mastersResearch_Jun_Sept2025/dimRed_P/Reducedstatistics/Reconstruction_error.py
@@ -0,0 +1,76 @@
+import numpy as np
+from sklearn.decomposition import PCA, NMF
+from pyspoc import ReducedStatistic
+from typing import Union
+
+
+class Reconstruction_error(ReducedStatistic):
+
+    def __init__(self, method = 'PCA', reduced_dimensionality = 2):
+
+        self.method = method
+        self.reduced_dimensionality = reduced_dimensionality
+
+        reducer_dict = {'PCA': PCA,
+                        'NMF': NMF}
+
+        self.reducer = reducer_dict[method](n_components=self.reduced_dimensionality)
+
+        # Calling base class initialiser.
+        super().__init__()
+
+    @property
+    def name(self) -> str:
+        return "Reconstruction_error"
+
+    @property
+    def identifier(self) -> str:
+        return "my_new_reducer_identifier"
+
+    @property
+    def labels(self) -> list[str]:
+        return ["my_new_reducer_label_1",
+                "my_new_reducer_label_2",
+                "my_new_reducer_label_n"]
+
+    def recon_MSE(self,X,X_reconstructed):
+        '''
+        Reconstruction relateive MSE
+        '''
+        assert X.shape == X_reconstructed.shape
+        return np.linalg.norm(X-X_reconstructed, ord='fro')**2/(X.shape[0]*X.shape[1])
+
+    def calculate_recon_error(self,X, X_reconstructed):
+        '''
+        Recontruction errors
+        mse : mean squared error between X and X_reconstructed
+        ams : mean square of X
+
+        Return:
+        rmse (=mse/ams) : relative mean squared error
+        '''
+        assert X.shape == X_reconstructed.shape
+
+        mse = self.recon_MSE(X, X_reconstructed) # mean square error
+        ams = self.recon_MSE(X, np.zeros(X.shape)) # mean square of original data matrix
+        rmse = mse/ams # relative mean square error
+
+        return rmse
+
+    def recon_rMSE(self,X, X_reconstructed):
+        '''
+        relative mean squared error
+        '''
+        return self.recon_MSE(X, X_reconstructed) / self.recon_MSE(X, np.zeros(X.shape))
+
+    def compute(self, data: np.ndarray) -> float:
+
+        # Dimensionally reduce the data and reconstruct the original one from the result
+        X = data
+        Z = self.reducer.fit_transform(X)
+        X_reconstructed = self.reducer.inverse_transform(Z)
+
+        # Compute the relative mean square error between the original dataset and the reconstructed one
+        rmse = self.calculate_recon_error(X, X_reconstructed)
+
+        return rmse
