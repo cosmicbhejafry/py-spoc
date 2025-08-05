793c0fd99090a0c9ffa7f42e88feea6d0e314a45
Pablo Saborido <143696174+pablo-saborido@users.noreply.github.com>
2025-08-05 09:22:55 +0100

    Changed all_coranking_metrics to the reducedstatistics folder, since it is now a reducedstatistic

--- working_analyses/mastersResearch_Jun_Sept2025/dimRed_P/Reducedstatistics/All_coranking_metrics.py
+++ working_analyses/mastersResearch_Jun_Sept2025/dimRed_P/Reducedstatistics/All_coranking_metrics.py
@@ -6,7 +6,7 @@ from scipy.stats import pearsonr, spearmanr
 from working_analyses.mastersResearch_Jun_Sept2025.dimRed_P.Reducedstatistics.coranking_auxiliary_functions import *
 
 
-class Coranking_metrics(ReducedStatistic):
+class All_coranking_metrics(ReducedStatistic):
 
     def __init__(self, method = 'PCA', reduced_dimensionality = 2, correlation_coefficient = 'Pearson'):
 
@@ -23,7 +23,7 @@ class Coranking_metrics(ReducedStatistic):
 
     @property
     def name(self) -> str:
-        return "Coranking_metrics"
+        return "All_coranking_metrics"
 
     @property
     def identifier(self) -> str:
